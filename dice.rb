548eefdc3611fa63516ce94ae93bb78259563d47
Lindsay Larkin Larkin <lindsay.larkin@gmail.com>
2017-01-31 10:02:11 -0700

    refactored dice

--- dice.rb
+++ dice.rb
@@ -4,16 +4,24 @@ class Dice
    roll
  end
 
- def roll
-   @die1 = 1 + rand(6)
-   @die2 = 1 + rand(6)
- end
-
- def show_dice
-   print "Die1: ", @die1, " Die2:", @die2
- end
-
- def show_sum
-   print "Sum of dice is ", @die1 + @die2, ".\n"
+ def roll(num_of_dice = 1)
+   if num_of_dice == 1
+       return 1 + rand(6)
+   else
+    dice_roll = []
+    num_of_dice.times { dice_roll << 1 + rand(6)}
+    return dice_roll
    end
+ end
 end
+
+#  def show_dice
+#    print "Die1: ", @die1, " Die2:", @die2
+#  end
+
+#  def show_sum
+#    print "Sum of dice is ", @die1 + @die2, ".\n"
+#    end
+
+
+
